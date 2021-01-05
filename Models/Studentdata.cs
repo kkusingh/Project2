807789d5997ab7333c4610999f8c5851878b222d
Kuldeep Singh <kusingh@my.harrisburgu.edu>
2021-01-04 19:41:19 -0500

    Adding assignment 2
    Adding assignment 2

---
+++ Models/Studentdata.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace Models
+{
+    public class Studentdata
+    {
+        [DisplayName("Student ID")]
+        public string Studentid { get; set; }
+
+        [DisplayName("Student Name")]
+
+        public string StudentName { get; set; }
+
+        [DisplayName("ISEM1 Marks")]
+        public string ISEM1 { get; set; }
+
+        [DisplayName("ISEM2 Marks")]
+
+        public string ISEM2 { get; set; }
+
+        [DisplayName("ISEM3 Marks")]
+        public string ISEM3 { get; set; }
+    }
+}
+
